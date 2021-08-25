d25b4b56172e1d7aa699efb2be1dc0d2429a8467
elena-krismer <70535771+elena-krismer@users.noreply.github.com>
2021-08-25 10:24:41 +0200

    gimme all three types

--- code/preprocessing/gimme.m
+++ code/preprocessing/gimme.m
@@ -7,37 +7,37 @@ changeCobraSolver ('glpk', 'all');
 humanone_model = readCbModel(which('/Users/s202425/Documents/GitHub/breast_metastasis/obj/models/Human-GEM.mat'));
 options = 'options_GIMME';
 load(['options_methods' filesep options]);
-tissuespecificmodel = createTissueSpecificModel(humanone_model, options);
 
 pathname = fileparts('/Users/s202425/Documents/GitHub/metastasis/obj/models/');
 
-load('/Users/s202425/Documents/GitHub/metastasis/obj/models/XXXX_brain_metastasis.mat');
+
 load('/Users/s202425/Documents/GitHub/metastasis/obj/models/lung_metastasis.mat');
-
 load('/Users/s202425/Documents/GitHub/metastasis/obj/models/brain_metastasis.mat');
+load('/Users/s202425/Documents/GitHub/metastasis/obj/models/MDA_MB_231.mat');
+
 % map expression to reaction
-[expressionRxns parsedGPR]= mapExpressionToReactions(humanone_model, brain_metastasis);
 % create GIMME model 
-threshold=round(prctile(expressionRxns, 25));
-XXXX_brain_metastasis = GIMME(tissuespecificmodel, expressionRxns, 0.5);
-matfile = fullfile(pathname, 'XXXX_brain_metastasis.mat');
-save(matfile, 'XXXX_brain_metastasis');
-
-
-% map expression to reaction
 [expressionRxns parsedGPR]= mapExpressionToReactions(humanone_model, lung_metastasis);
-% create GIMME model 
-threshold=round(prctile(expressionRxns, 25));
-lung_metastasis = GIMME(tissuespecificmodel, expressionRxns, threshold);
+options.expressionRxns = expressionRxns;
+options.threshold = round(prctile(expressionRxns, 25));
+lung_metastasis = createTissueSpecificModel(humanone_model, options);
 matfile = fullfile(pathname, 'lung_metastasis.mat');
 save(matfile, 'lung_metastasis');
 
- 
-load('/Users/s202425/Documents/GitHub/metastasis/obj/models/brain_metastasis.mat');
+
 % map expression to reaction
 [expressionRxns parsedGPR]= mapExpressionToReactions(humanone_model, brain_metastasis);
 % create GIMME model 
-threshold=round(prctile(expressionRxns, 25));
-brain_metastasis = GIMME(tissuespecificmodel, expressionRxns, threshold);
+options.expressionRxns = expressionRxns;
+options.threshold = round(prctile(expressionRxns, 25));
+brain_metastasis  = createTissueSpecificModel(humanone_model, options);
 matfile = fullfile(pathname, 'brain_metastasis.mat');
 save(matfile, 'brain_metastasis');
+
+[expressionRxns parsedGPR]= mapExpressionToReactions(humanone_model, MDA_MB_231);
+% create GIMME model 
+options.expressionRxns = expressionRxns;
+options.threshold = round(prctile(expressionRxns, 25));
+MDA_MB_231  = createTissueSpecificModel(humanone_model, options);
+matfile = fullfile(pathname, 'MDA_MB_231.mat');
+save(matfile, 'MDA_MB_231');
